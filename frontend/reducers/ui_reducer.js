f87fbc5d1f45a5ebba3e055435e25277ba0c7c4c
Joe Nicholson <joeqnicholson@gmail.com>
2019-12-11 12:13:16 -0500

    fix ui error

--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,5 +1,5 @@
 import modalReducer from './modal_reducer'
-import { combineReducers } from '../../../../Library/Caches/typescript/3.6/node_modules/redux'
+import { combineReducers } from 'redux'
 const uiReducer=combineReducers({
     modal: modalReducer
 })
