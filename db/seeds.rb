45d57b54845cdbc9c806703e2064eb75a046a006
Joe Nicholson <joeqnicholson@gmail.com>
2019-12-16 17:23:57 -0500

    heroku aws seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
 
-Video.create!({:user_id => 1, :title => 'charlie brown', :description => 'lucy and the pumpkin', :plays => 0})
+video = Video.create!({:user_id => 1, :title => 'charlie brown', :description => 'lucy and the pumpkin', :plays => 0})
+thumbnail_1 = open("https://grimeo-seeds.s3.amazonaws.com/MZk6Vohdz5qKgRHikNRgcYkh.png")
+video_1 = open("https://grimeo-seeds.s3.amazonaws.com/charlie_brown+(1).mp4")
+video.thumbnail.attach(io: thumbnail_1, filename: 'MZk6Vohdz5qKgRHikNRgcYkh.png')
+video.video.attach(io: video_1, filename: 'charlie_brown+(1).mp4')
+video.save
+
+
 Comment.create!({:user_id => 1, :body => 'i love charlie!!', :video_id => 3})
 Comment.create!({:user_id => 15, :body => 'i hate charlie!!', :video_id => 3})
 Comment.create!({:user_id => 4, :body => 'go charlie!!', :video_id => 3})
